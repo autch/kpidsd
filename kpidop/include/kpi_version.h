07553e186d977fdc59634e0fb437e70494b74f19
Autch <autch@autch.net>
2015-07-06 02:26:32 +0900

    kpiのバージョン情報を修正

--- kpidop/include/kpi_version.h
+++ kpidop/include/kpi_version.h
@@ -4,5 +4,4 @@
 #define KPI_VERSION		(1 << 8 | 0) // 1.0
 #define KPI_DESC		"DSD over PCM plugin for KbMedia Player"
 #define KPI_COPYRIGHT	"Copyright(c) 2015, Autch.net; " \
-"libid3tag Copyright(C) 2000 - 2004 Underbit Technologies, Inc; " \
-"zlib Copyright (C) 1995-2010 Jean-loup Gailly and Mark Adler"
+"taglib Copyright (C) 2002 - 2008 by Scott Wheeler, et al."
