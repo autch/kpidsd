1051525f7aac0e285d12d23b659f9a2a92d4085a
Autch <autch@autch.net>
2015-07-12 00:04:36 +0900

    kpidop.kpi v1.0.0

--- kpidop/include/kpi_version.h
+++ kpidop/include/kpi_version.h
@@ -1,6 +1,13 @@
 
 #pragma once
 
-#define KPI_VERSION		(1 << 8 | 0) // 1.0
-#define KPI_DESC		"DSD over PCM plugin for KbMedia Player"
+#define MAKE_SEMVER(major, minor, patch)	(((major) << 16) | ((minor) << 8) | (patch))
+#ifdef _DEBUG
+#define DEBUG_MSG		" [DEBUG]"
+#else
+#define DEBUG_MSG		""
+#endif
+
+#define KPI_VERSION		MAKE_SEMVER(1, 0, 0)
+#define KPI_DESC		"DSD over PCM plugin for KbMedia Player" DEBUG_MSG
 #define KPI_COPYRIGHT	"Copyright(c) 2015, Autch.net"
